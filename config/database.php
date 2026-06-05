bbc2e6535bea8a80e26002d5d7006e3f43afa50c
Chihab Lachgari <clachgari1@gmail.com>
2026-06-05 21:49:19 +0200

    Fix Railway database config

--- config/database.php
+++ config/database.php
@@ -45,24 +45,18 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+    'driver' => 'mysql',
+    'url' => env('DATABASE_URL'),
+
+    'host' => env('DB_HOST', null),
+    'port' => env('DB_PORT', null),
+    'database' => env('DB_DATABASE', null),
+    'username' => env('DB_USERNAME', null),
+    'password' => env('DB_PASSWORD', null),
+
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+],
 
         'mariadb' => [
             'driver' => 'mariadb',
