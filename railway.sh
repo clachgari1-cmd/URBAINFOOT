eb6d3fa319d7d52c78c04da4337314c54f5e7a4b
Chihab Lachgari <clachgari1@gmail.com>
2026-06-05 22:04:49 +0200

    Add debug logs and set -e to railway.sh

--- railway.sh
+++ railway.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -e
 
+echo "=== Lancement des migrations Laravel ==="
 php artisan migrate --force
+
+echo "=== Lancement des seeders (Données de test) ==="
 php artisan db:seed --force
+
+echo "=== Nettoyage du cache ==="
 php artisan config:clear
 php artisan cache:clear
 
+echo "=== Démarrage de l'application ==="
 php artisan serve --host=0.0.0.0 --port=$PORT
